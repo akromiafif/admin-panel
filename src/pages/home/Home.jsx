39cce986fe8df2803dfc0c528c5316ac6b727127
Afif Akromi <massachusetts48@gmail.com>
2022-04-30 03:25:28 +0700

    add scss

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="title">Hello World</h1>
+    </div>
+  );
+};
+
+export default Home;
